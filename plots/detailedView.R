82434408320dd05f4148dcedbc11b49e3c84e84b
Bastian Mannerow <bastian.mannerow@student.uni-luebeck.de>
2024-04-30 00:30:56 +0200

    Erhöhung der Wartbarkeit des Codes

---
+++ plots/detailedView.R
@@ -0,0 +1,41 @@
+
+
+generateTimeSeriesPlot <- function(soll_values, ist_values, soll_anomalies, ist_anomalies, plot_font_family, normal_text_font_size){
+  soll_values$Anomalie <- ifelse(soll_values$year %in% soll_anomalies$Jahr, "Soll-Anomalie", "Soll-Werte")
+  
+  timeSeriesPlot <- ggplot(soll_values, aes(x = year, y = value_soll)) +
+    geom_bar(aes(fill = Anomalie), stat = "identity", width = 0.7, show.legend = TRUE) +
+    geom_line(data = ist_values, aes(x = year, y = value_ist, color = "Ist-Werte", group = 1), size = 2) +
+    geom_point(data = ist_anomalies, aes(x = Jahr, y = Wert, color = "Ist-Anomalie"), size = 5) +
+    scale_fill_manual(values = c("Soll-Werte" = "#d3d3d3", "Soll-Anomalie" = "#841919"), name = "Soll-Werte") +
+    scale_color_manual(values = c("Ist-Werte" = "#197084", "Ist-Anomalie" = "#841919"), name = "Ist-Werte") +
+    labs(y = "Absolutwerte") +
+    theme_minimal() +
+    theme(text = element_text(family = plot_font_family), 
+          axis.text.y = element_text(size = normal_text_font_size),
+          axis.title.x = element_blank(), 
+          axis.text.x = element_blank(),
+          legend.text = element_text(size = normal_text_font_size),
+          legend.title = element_text(size = normal_text_font_size), 
+          axis.title.y = element_text(size = normal_text_font_size),
+          axis.ticks.x = element_blank())
+}
+
+generateDifferencePlot <- function(combined_df, diff_anomalies, plot_font_family, normal_text_font_size){
+  # Illiminate false anomaly entries
+  combined_df$Anomalie <- ifelse(combined_df$year %in% diff_anomalies$Jahr, "Anomalie", "Negative Differenz")
+  
+  detailPlot <- ggplot(combined_df, aes(x = year, y = difference, fill = Anomalie)) +
+    geom_bar(stat = "identity", aes(fill = ifelse(difference > 0, "Positive Differenz", Anomalie))) +
+    geom_hline(yintercept = 0, linetype = "dashed") +
+    scale_fill_manual(values = c("Anomalie" = "#841919", "Positive Differenz" = "#28841980", "Negative Differenz" = "#84191980"), name = "Differenz: Soll - Ist") +
+    labs(y = "Zieldifferenz") +
+    theme_minimal() +
+    theme(text = element_text(family = plot_font_family), 
+          axis.text.x = element_text(size = normal_text_font_size),
+          axis.text.y = element_text(size = normal_text_font_size),
+          legend.text = element_text(size = normal_text_font_size),
+          legend.title = element_text(size = normal_text_font_size),
+          axis.title.y = element_text(size = normal_text_font_size),
+          axis.title.x = element_blank())
+}
--- server.R
+++ server.R
@@ -16,6 +16,9 @@ library(fontawesome)
 library(patchwork)
 library(shinyjs)
 
+# own methods
+source("plots/detailedView.R")
+
 ### load Roboto font and change scale view
 font_add_google("Roboto Condensed", family = "Roboto")
 showtext_auto()
@@ -40,6 +43,12 @@ df_kapitel <- read_csv("./Data/hh_sh_ep14_kapitel.csv", col_types = cols(Kapitel
 df_zweck <- read.table("./Data/hh_sh_ep14_zweck.csv", sep = ",", header = TRUE, 
                        fileEncoding = "UTF-8", 
                        colClasses = c(Kapitel="character", Gesamttitel="character"))
+
+# Should be moved into an own method, but here we are...
+#df_ist <- df_ist %>% mutate(Gesamttitel = paste(substr(Gesamttitel,1,4), substr(Gesamttitel,5,6), substr(Gesamttitel,7,9), sep = " "))
+#df_soll <- df_soll %>% mutate(Gesamttitel = paste(substr(Gesamttitel,1,4), substr(Gesamttitel,5,6), substr(Gesamttitel,7,9), sep = " "))
+#df_diff <- df_diff %>% mutate(Gesamttitel = paste(substr(Gesamttitel,1,4), substr(Gesamttitel,5,6), substr(Gesamttitel,7,9), sep = " "))
+#df_zweck <- df_zweck %>% mutate(Gesamttitel = paste(substr(Gesamttitel,1,4), substr(Gesamttitel,5,6), substr(Gesamttitel,7,9), sep = " "))
 
 # Adding the column Anomalies for each year to be able to distinguish between AI and Human anomaly
 df_ist <- df_ist %>%
@@ -253,7 +262,6 @@ shinyServer(function(input, output, session) {
             select(Zweckbestimmung)
           
           title_with_breaks <- insert_breaks_every_n_chars(paste(title, " - ", purpose))
-          
           # Get Data and removes Anomalie in year, whch ensures a clean x axis.
           ist_values <- df_ist %>%
             filter(Gesamttitel == title) %>%
@@ -281,7 +289,6 @@ shinyServer(function(input, output, session) {
           # Import anomalies
           anomalies <- subset(rv$x, Titel == title)
           
-          
           soll_anomalies <- anomalies %>% 
             filter(Art == "Soll")
           
@@ -291,44 +298,16 @@ shinyServer(function(input, output, session) {
           diff_anomalies <- anomalies %>% 
             filter(Art == "Diff")
           
+          
+          
           # Time Series
-          soll_values$Anomalie <- ifelse(soll_values$year %in% soll_anomalies$Jahr, "Soll-Anomalie", "Soll-Werte")
+          timeSeriesPlot = generateTimeSeriesPlot(soll_values, ist_values, soll_anomalies, ist_anomalies, plot_font_family, normal_text_font_size)
           
-          timeSeriesPlot <- ggplot(soll_values, aes(x = year, y = value_soll)) +
-            geom_bar(aes(fill = Anomalie), stat = "identity", width = 0.7, show.legend = TRUE) +
-            geom_line(data = ist_values, aes(x = year, y = value_ist, color = "Ist-Werte", group = 1), size = 2) +
-            geom_point(data = ist_anomalies, aes(x = Jahr, y = Wert, color = "Ist-Anomalie"), size = 5) +
-            scale_fill_manual(values = c("Soll-Werte" = "#d3d3d3", "Soll-Anomalie" = "#841919"), name = "Soll-Werte") +
-            scale_color_manual(values = c("Ist-Werte" = "#197084", "Ist-Anomalie" = "#841919"), name = "Ist-Werte") +
-            labs(y = "Absolutwerte") +
-            theme_minimal() +
-            theme(text = element_text(family = plot_font_family), 
-                  axis.text.y = element_text(size = normal_text_font_size),
-                  axis.title.x = element_blank(), 
-                  axis.text.x = element_blank(),
-                  legend.text = element_text(size = normal_text_font_size),
-                  legend.title = element_text(size = normal_text_font_size), 
-                  axis.title.y = element_text(size = normal_text_font_size),
-                  axis.ticks.x = element_blank())
+          # Difference Plot (Second Half)
+          differencePlot = generateDifferencePlot(combined_df, diff_anomalies, plot_font_family, normal_text_font_size)
           
-          # Illiminate false anomaly entries
-          combined_df$Anomalie <- ifelse(combined_df$year %in% diff_anomalies$Jahr, "Anomalie", "Negative Differenz")
-          
-          detailPlot <- ggplot(combined_df, aes(x = year, y = difference, fill = Anomalie)) +
-            geom_bar(stat = "identity", aes(fill = ifelse(difference > 0, "Positive Differenz", Anomalie))) +
-            geom_hline(yintercept = 0, linetype = "dashed") +
-            scale_fill_manual(values = c("Anomalie" = "#841919", "Positive Differenz" = "#28841980", "Negative Differenz" = "#84191980"), name = "Differenz: Soll - Ist") +
-            labs(y = "Zieldifferenz") +
-            theme_minimal() +
-            theme(text = element_text(family = plot_font_family), 
-                  axis.text.x = element_text(size = normal_text_font_size),
-                  axis.text.y = element_text(size = normal_text_font_size),
-                  legend.text = element_text(size = normal_text_font_size),
-                  legend.title = element_text(size = normal_text_font_size),
-                  axis.title.y = element_text(size = normal_text_font_size),
-                  axis.title.x = element_blank())
-          
-          combinedPlot <- timeSeriesPlot / detailPlot + plot_layout(guides = "collect") + plot_annotation(title = title_with_breaks) + theme(plot.margin = margin(1, 1, 1, 1), plot.title = element_text(size = mini_headline_font_size, family = plot_font_family))
+          # Combine the Plots
+          combinedPlot <- timeSeriesPlot / differencePlot + plot_layout(guides = "collect") + plot_annotation(title = title_with_breaks) + theme(plot.margin = margin(1, 1, 1, 1), plot.title = element_text(size = mini_headline_font_size, family = plot_font_family))
           
           return(combinedPlot)
         })
@@ -497,7 +476,7 @@ shinyServer(function(input, output, session) {
   observe({
     width <- input$windowSize$width
     height <- input$windowSize$height
-    message(paste("Aktuelle Fenstergröße: Breite =", width, "Höhe =", height))
+    # message(paste("Aktuelle Fenstergröße: Breite =", width, "Höhe =", height))
   })
   
   #------------------------------------------------------------------------------
@@ -624,6 +603,7 @@ shinyServer(function(input, output, session) {
   
   # initialize a tibble with anomalies from AI output (artifical data, not real)
   df_new <- read_csv("./Data/hh_sh_ep14_fakeAI.csv", col_types = "cccdcc")
+  
   # add the icons
   df_new <- df_new %>% mutate(Ursprung = if_else(startsWith(Ursprung, "User"),
                                                paste(fa("user"), "Nutzer"),
@@ -706,7 +686,6 @@ shinyServer(function(input, output, session) {
     selected_points <- selected_points %>%
       mutate(Ursprung = str_detect(Ursprung, "Nutzer"))
     
-    print(selected_points)
     return(selected_points)
   })
   
